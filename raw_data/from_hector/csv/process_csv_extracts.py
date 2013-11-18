df6a8a51f5a34e8a17b548029b3fb3affc284e8e
Thomas Hladish <tjhladish@utexas.edu>
2013-11-18 16:02:07 -0500

    Updating process_csv_extracts.py
    These are updates to the script that process the csv extracts of the Excel files
    from Hector.  I'm not sure why they weren't committed before, but they look
    legit.

--- raw_data/from_hector/csv/process_csv_extracts.py
+++ raw_data/from_hector/csv/process_csv_extracts.py
@@ -47,16 +47,7 @@ def match_locality(string, localities):
     for s in substring_hits:
         if (s['loc_name'],s['muni_name']) not in jaro_hits:
             matches.append(s)
-    #print matches
     return matches
-
-
-#def remove_funny_chars(string):
-#    newstr = []
-#    for c in string.lower():
-#        if c in lowercase:
-#            newstr.append(c)
-#    return ''.join(newstr)
 
 
 def find_idxs_of_max(scores):
@@ -72,29 +63,22 @@ def find_idxs_of_max(scores):
     return idx
 
 
-#def guess_again(orig_string, matches):
-#    ''' Sometimes the mismatch is due to inconsistent use of
-#    accent marks.  See if any of the matches are particularly
-#    close when accent marks are disregarded '''
-#    alt_scores = []
-#    string = remove_funny_chars(orig_string) 
-#    for m in matches:
-#        alt_scores.append(distance(string, remove_funny_chars(m['loc_name'])))
-#    return alt_scores
-
-
 # import locality --> municipality map (many-to-one mapping)
 localities = [] 
 for line in file("/home/tjhladish/work/dengue/raw_data/yucatan_locality_names_and_numbers"):
     parts = line.strip().split(',')
-    localities.append(dict(zip(['state_num', 'state_name', 'muni_num', 'muni_name', 'loc_num', 'loc_name'], parts)))
+    if len(parts) != 8:
+        print line
+    localities.append(dict(zip(['state_num', 'state_name', 'muni_num', 'muni_name', 'loc_num', 'loc_name', 'altitude', 'population'], parts)))
 
 total_cases_filenames = sorted(glob('*t.csv'))
-fo = open("total_yucatan_cases.csv", 'w')
+#total_cases_filenames = ['confirmados_2001t.csv']
+#fo = open("total_yucatan_cases.csv", 'w')
 
+all_matches = dict()
 for filename in total_cases_filenames:
-    year = filename[12:16]
-    #print year
+    print filename
+    year = filename[12:16] # Example: confirmados_2009t.csv
     start = False
     for line in file(filename):
         parts = line.strip().split(';')
@@ -105,32 +89,39 @@ for filename in total_cases_filenames:
             #print "  Ending Yucatan"
             start = False
         elif start:
-            matches = match_locality(parts[0], localities)
-#            print parts[0], year
-            best_guesses = [0] 
-            scores = [m['score'] for m in matches]
-            alt_scores = ['NA']
-            if len(matches) > 1:
-                #alt_scores = guess_again(parts[0], matches)
-                best_guesses = find_idxs_of_max(scores)
-            for i in range(len(matches)):
-                if i == 0:
-                    print year,';', parts[0],';',
-                else:
-                    print ' ; ;',
-                match = matches[i]
-                print match['loc_name'],';', match['loc_num'],';', match['muni_name'],';', match['muni_num'],';', match['score'], 
-                #print '\t', match['score'], alt_scores[i], match['loc_num'], match['loc_name'], match['muni_num'], match['muni_name'],
-                if len(matches) == 1:
-                    print "; ***"
-                elif len(best_guesses) == 1 and best_guesses[0] == i:
-                    print "; **"
-                elif i in best_guesses:
-                    print "; *"
-                else:
-                    print "; "
-            #fo.write(year + ',' + ','.join(parts[:53]) + '\n')
-            #print
+            if parts[0] not in all_matches:
+                matches = match_locality(parts[0], localities)
+                all_matches[parts[0]] = dict()
+                all_matches[parts[0]]['hits'] = matches
+                all_matches[parts[0]]['years'] = [year]
+            else:
+                all_matches[parts[0]]['years'].append(year)
         else:
             continue
-fo.close()
+
+for loc in all_matches:
+#    print all_matches[loc]
+    matches = all_matches[loc]['hits']
+    years   = all_matches[loc]['years']
+    best_guesses = [0] 
+    scores = [m['score'] for m in matches]
+    if len(matches) > 1:
+        best_guesses = find_idxs_of_max(scores)
+    for i in range(len(matches)):
+        if i == 0:
+            print loc,';',
+        else:
+            print ' ;',
+        match = matches[i]
+        print match['loc_name'],';', match['loc_num'],';', match['muni_name'],';', match['muni_num'],';', match['population'],';', match['score'], 
+        if len(matches) == 1:
+            print "; *** ;",
+        elif len(best_guesses) == 1 and best_guesses[0] == i:
+            print "; ** ;",
+        elif i in best_guesses:
+            print "; * ;",
+        else:
+            print "; ;",
+        print ','.join(years)
+    #fo.write(year + ',' + ','.join(parts[:53]) + '\n')
+#fo.close()
