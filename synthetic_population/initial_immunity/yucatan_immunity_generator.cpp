fd32ed995909de518e5f43cb7473cf42bd6222d7
Thomas Hladish <tjhladish@ufl.edu>
2014-08-13 02:42:40 -0400

    Modified standalone EF estimator to use simple ABC

--- synthetic_population/initial_immunity/yucatan_immunity_generator.cpp
+++ synthetic_population/initial_immunity/yucatan_immunity_generator.cpp
@@ -215,7 +215,7 @@ year 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 2
 */
     map<int, vector<AgeTally> > census;
     ifstream censusfile(filename);
-
+    // We need to add a check in case the file doesn't exist
     string line; 
     vector<string> ages;
     while(getline(censusfile,line)) {
@@ -227,7 +227,7 @@ year 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 2
             continue;
         } else if (fields.size()) {          
             year = stoi(fields[0]);
-            for(int i=1; i < fields.size(); i++ ) {  
+            for(unsigned int i=1; i < fields.size(); i++ ) {  
                 census[year].push_back( AgeTally(ages[i-1],stoi(fields[i])) );
                 //cerr << year << " " << ages[i-1] << " " << fields[i] << endl;
             }
@@ -253,24 +253,21 @@ int age_str_to_int(string age_str) {
     int age;
     try {
         age = stoi(age_str);
-    //} catch (const std::invalid_argument& ia ) {
-    //} catch (const std::string& ia ) {
-    //} catch (...) {
-    } catch (const std::exception &e) {
+    } catch (const std::invalid_argument& ia ) {
         age = MAX_CENSUS_AGE;
     }
     return age;
 }
 
 
-vector< vector< vector<int> > > initialize_full_population(map<int,vector<AgeTally> >& census, int first_year) {
+vector< vector< vector<int> > > initialize_full_population(const map<int,vector<AgeTally> >& census, int first_year) {
     vector< vector< vector<int> > > full_pop(MAX_CENSUS_AGE + 1, vector< vector<int> >());
     //full_pop = [[] for i in range(MAX_CENSUS_AGE + 1)]
 
-    for (unsigned int i = 0; i < census[first_year].size(); ++i) {
-        string age_str = census[first_year][i].age;
+    for (unsigned int i = 0; i < census.at(first_year).size(); ++i) {
+        string age_str = census.at(first_year)[i].age;
         int age = age_str_to_int(age_str);
-        for (int k = 0; k < census[first_year][i].tally; ++k) {
+        for (int k = 0; k < census.at(first_year)[i].tally; ++k) {
             full_pop[age].push_back(vector<int>(NUM_SEROTYPES,0));
         }
     }
@@ -288,7 +285,7 @@ void age_immunity(vector< vector< vector<int> > >& pop) {
 }
 
 
-int extract_tally(vector<AgeTally> census_year, string age_str) {
+int extract_tally(const vector<AgeTally> census_year, string age_str) {
     for (unsigned int i = 0; i < census_year.size(); ++i) { 
         if (census_year[i].age == age_str) return census_year[i].tally;
     }
@@ -296,7 +293,7 @@ int extract_tally(vector<AgeTally> census_year, string age_str) {
 }
 
 
-void age_full_population(map<int,vector<AgeTally> >& census, vector< vector< vector<int> > >& full_pop, int new_year) {
+void age_full_population(const map<int,vector<AgeTally> >& census, vector< vector< vector<int> > >& full_pop, int new_year) {
 /*
 "Age" the population by sampling from younger age classes as necessary to make up
 the census numbers for the new year, e.g. if there are ten 30-year-olds in 1980,
@@ -320,14 +317,14 @@ than the last.
 */
     age_immunity(full_pop);
     vector<string> age_keys;
-    for (unsigned int a = 0; a < census[new_year].size(); ++a) age_keys.push_back(census[new_year][a].age);
+    for (unsigned int a = 0; a < census.at(new_year).size(); ++a) age_keys.push_back(census.at(new_year)[a].age);
     reverse(age_keys.begin(),age_keys.end());
 
     
     for (unsigned int i = 0; i < age_keys.size(); ++i) { // going from oldest to youngest
         string age_str = age_keys[i];
         int age = age_str_to_int(age_str);
-        int tally = extract_tally(census[new_year], age_str);
+        int tally = extract_tally(census.at(new_year), age_str);
         if (age == 0) {
             // nothing to inherit for infants
             full_pop[age] = vector< vector<int> > (tally, vector<int> (NUM_SEROTYPES,0));
@@ -388,123 +385,126 @@ void output_immunity_file(string filename, const vector<vector<vector<int> > >& 
 }
 
 
-vector<vector<vector<int>>> simulate_immune_dynamics(const float EXPANSION_FACTOR, const int FIRST_YEAR) {
+int main(int argc, char** argv) {
     const bool bootstrap_data = true;
+    const bool write_immunity_file = false;
+
+    if (argc != 2) {
+        cerr << "\n\tUsage: ./immgen <last_year_to_simulate>\n\n";
+        //cerr << "\n\tUsage: ./immgen <expansion_factor> <last_year_to_simulate>\n\n";
+        exit(100);
+    }
 
     gsl_rng_set(RNG, seedgen());
-    const float EXPANSION_FACTOR = atof(argv[1]);
+    //const float EXPANSION_FACTOR = atof(argv[1]);
     const int FIRST_YEAR = 1979;
-    const int LAST_YEAR = atoi(argv[2]);
+    const int LAST_YEAR = atoi(argv[1]);
+    const int MAX_EF = 50;
+    const int MIN_EF = 25;
 
     vector<int> YEARS(LAST_YEAR - FIRST_YEAR + 1);
     iota(YEARS.begin(), YEARS.end(), FIRST_YEAR);
 
-    vector<int> cases;
-    vector<vector<double>> serotype_wt;
-    
-    if (bootstrap_data) {
-        cases = bootstrap_cases(EXPANSION_FACTOR);
-        serotype_wt = bootstrap_serotypes(cases);
-    } else {
-        cases = CASES;
-        serotype_wt = SEROTYPE_WT;
-    }
+    const map<int, vector<AgeTally> > census = import_census_data("interpolated_ages-yucatan.out");
 
-    /*for (unsigned int y = 0; y < SEROTYPE_WT.size(); ++y) {
-        cout << setprecision(4);
-        for ( double w: SEROTYPE_WT[y] ) cout << w << " ";
-        cout << endl;
-        for ( double w: serotype_wt[y] ) cout << w << " ";
-        cout << endl << endl;
-    }*/
+    const int REPS = 10000;
 
-    map<int, vector<AgeTally> > census = import_census_data("interpolated_ages-yucatan.out");
-    vector<vector<vector<int> > > full_pop = initialize_full_population(census, FIRST_YEAR);
+    for (int rep = 0; rep < REPS; ++rep) {
 
-    int year = 0;
-    int kids_in_1987 = 0; // in the 8-14 range
-    float seropositive_kids_in_1987 = 0;  // looking to see ~60% of the population
-
-    for (unsigned int ya = YEARS.size(); ya > 0; --ya) { 
-        if (year > 0) age_full_population(census, full_pop, YEARS[year]);
-        int num_of_infections = (int) (cases[year] * EXPANSION_FACTOR + 0.5);
-
-        vector<int> pop_size_by_age = tally_pop_by_age(full_pop);
-        int pop_size = accumulate(pop_size_by_age.begin(), pop_size_by_age.end(), 0);
-        cout << "year, years ago, pop, cases, infections, expansion factor\n";
-        cout << YEARS[year] << " " <<  ya << " " <<  pop_size << " " <<  cases[year] << " " <<  num_of_infections << " " <<  EXPANSION_FACTOR << endl;
-        cout << "expected serotype distribution: "; for (double wt: serotype_wt[year]) cout << wt << " "; cout << endl;
-        vector<int> serotype_tally(NUM_SEROTYPES, 0);
-        for (int i = 0; i < num_of_infections; ++i) {
-            int s = sample_serotype(serotype_wt[year]);
-            bool infection_occurred = false;
-            while (not infection_occurred) { 
-                int test = gsl_rng_uniform_int(RNG, pop_size);
-                int age = 0;
-                while (test >= pop_size_by_age[age]) {
-                    test -= pop_size_by_age[age];
-                    age++;
-                }
-                if (full_pop[age][test][s] == 0 and not recently_infected(full_pop[age][test])) {
-                    // infect test person
-                    full_pop[age][test][s] = 1;
-                    serotype_tally[s]++;
-                    infection_occurred = true;
-                }
-            }
-        }
-        cout << "actual serotype counts: "; for (int t: serotype_tally) cout << t << " "; cout << endl;
-        if (num_of_infections > 0) {
-            const double total = (double) sum(serotype_tally);
-            cout << "actual serotype frequency: "; for(int c: serotype_tally)  cout << c/total << " "; cout << endl;
-        } else {
-            cout << "actual serotype frequency: NA NA NA NA\n";
-        }
+        const float EXPANSION_FACTOR = gsl_rng_uniform(RNG)*(MAX_EF - MIN_EF) + MIN_EF;
+        vector<int> cases;
+        vector<vector<double>> serotype_wt;
+        vector<vector<vector<int> > > full_pop = initialize_full_population(census, FIRST_YEAR);
         
-        if (YEARS[year] == 1987) {
-            for (int age = 8; age < 15; ++age) {
-                for (auto kid: full_pop[age]) {
-                //for (vector<int> kid: full_pop[age]) {
-                    kids_in_1987 += 1.0;
-                    auto seropos = sum(kid);
-                    if (seropos > 0) {
-                        seropositive_kids_in_1987 += 1.0;
-                    }
-                 }
-            }
-            cout << "Number of kids 8-14 in 1987: " << kids_in_1987 << endl;
-            cout << "Number of seropositive kids 8-14 in 1987: " << seropositive_kids_in_1987 << endl;
-            cout << "Fraction seropositive: " << seropositive_kids_in_1987/kids_in_1987 << endl;
-            cout << "Empirical fraction seropositive: ~0.6" << endl;
-            cout << "Estimated expansion factor correction: " << 0.6/(seropositive_kids_in_1987/kids_in_1987) << endl;
-            cerr << EXPANSION_FACTOR << " " << seropositive_kids_in_1987/kids_in_1987 << endl;
+        if (bootstrap_data) {
+            cases = bootstrap_cases(EXPANSION_FACTOR);
+            serotype_wt = bootstrap_serotypes(cases);
+        } else {
+            cases = CASES;
+            serotype_wt = SEROTYPE_WT;
         }
-        year++;
-        cout << endl;
+
+        /*for (unsigned int y = 0; y < SEROTYPE_WT.size(); ++y) {
+            cout << setprecision(4);
+            for ( double w: SEROTYPE_WT[y] ) cout << w << " ";
+            cout << endl;
+            for ( double w: serotype_wt[y] ) cout << w << " ";
+            cout << endl << endl;
+        }*/
+
+        int year = 0;
+        int kids_in_1987 = 0; // in the 8-14 range
+        float seropositive_kids_in_1987 = 0;  // looking to see ~60% of the population
+
+        for (unsigned int ya = YEARS.size(); ya > 0; --ya) { 
+            if (year > 0) age_full_population(census, full_pop, YEARS[year]);
+            int num_of_infections = (int) (cases[year] * EXPANSION_FACTOR + 0.5);
+
+            vector<int> pop_size_by_age = tally_pop_by_age(full_pop);
+            int pop_size = accumulate(pop_size_by_age.begin(), pop_size_by_age.end(), 0);
+            cout << "year, years ago, pop, cases, infections, expansion factor\n";
+            cout << YEARS[year] << " " <<  ya << " " <<  pop_size << " " <<  cases[year] << " " <<  num_of_infections << " " <<  EXPANSION_FACTOR << endl;
+            cout << "expected serotype distribution: "; for (double wt: serotype_wt[year]) cout << wt << " "; cout << endl;
+            vector<int> serotype_tally(NUM_SEROTYPES, 0);
+            for (int i = 0; i < num_of_infections; ++i) {
+                int s = sample_serotype(serotype_wt[year]);
+                bool infection_occurred = false;
+                while (not infection_occurred) { 
+                    int test = gsl_rng_uniform_int(RNG, pop_size);
+                    int age = 0;
+                    while (test >= pop_size_by_age[age]) {
+                        test -= pop_size_by_age[age];
+                        age++;
+                    }
+                    if (full_pop[age][test][s] == 0 and not recently_infected(full_pop[age][test])) {
+                        // infect test person
+                        full_pop[age][test][s] = 1;
+                        serotype_tally[s]++;
+                        infection_occurred = true;
+                    }
+                }
+            }
+            cout << "actual serotype counts: "; for (int t: serotype_tally) cout << t << " "; cout << endl;
+            if (num_of_infections > 0) {
+                const double total = (double) sum(serotype_tally);
+                cout << "actual serotype frequency: "; for(int c: serotype_tally)  cout << c/total << " "; cout << endl;
+            } else {
+                cout << "actual serotype frequency: NA NA NA NA\n";
+            }
+            
+            if (YEARS[year] == 1987) {
+                for (int age = 8; age < 15; ++age) {
+                    for (auto kid: full_pop[age]) {
+                    //for (vector<int> kid: full_pop[age]) {
+                        kids_in_1987 += 1.0;
+                        auto seropos = sum(kid);
+                        if (seropos > 0) {
+                            seropositive_kids_in_1987 += 1.0;
+                        }
+                     }
+                }
+                cout << "Number of kids 8-14 in 1987: " << kids_in_1987 << endl;
+                cout << "Number of seropositive kids 8-14 in 1987: " << seropositive_kids_in_1987 << endl;
+                cout << "Fraction seropositive: " << seropositive_kids_in_1987/kids_in_1987 << endl;
+                cout << "Empirical fraction seropositive: ~0.6" << endl;
+                cout << "Estimated expansion factor correction: " << 0.6/(seropositive_kids_in_1987/kids_in_1987) << endl;
+                cerr << EXPANSION_FACTOR << " " << seropositive_kids_in_1987/kids_in_1987 << endl;
+            }
+            year++;
+            cout << endl;
+        }
+        cout << "\t\t\tDone.\n";
+
+        //string immunity_filename = "/work/01856/thladish/initial_immunity/" + to_string(EXPANSION_FACTOR) + ".txt";
+        if (write_immunity_file) {
+            string immunity_filename = to_string(EXPANSION_FACTOR) + ".txt";
+            output_immunity_file(immunity_filename, full_pop);
+        }
+
+        cout << "\t\t\tDone.\n";
     }
-    cout << "\t\t\tDone.\n";
-    
-    return full_pop;
-}
-
-
-int main(int argc, char** argv) {
-    const bool write_immunity_file = false;
-    if (argc != 3) {
-        cerr << "\n\tUsage: ./immgen <expansion_factor> <last_year_to_simulate>\n\n";
-        exit(100);
-    }
-
-    vector<vector<vector<int>>> full_pop = simulate_immune_dynamics(argv[1], argv[2]);
-
-    //string immunity_filename = "/work/01856/thladish/initial_immunity/" + to_string(EXPANSION_FACTOR) + ".txt";
-    if (write_immunity_file) {
-        string immunity_filename = to_string(static_cast<long double>(EXPANSION_FACTOR)) + ".txt";
-        output_immunity_file(immunity_filename, full_pop);
-    }
-
-    cout << "\t\t\tDone.\n";
 
     return 0;
 }
+
 
