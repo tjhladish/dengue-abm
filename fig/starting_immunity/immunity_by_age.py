ec710f13fc5d2851289afb65e7b817b073d13956
Tom Hladish <tjhladish@ufl.edu>
2014-11-20 19:53:04 -0500

    Modifying immunity vs age scripts to process multiple immunity input files

--- fig/starting_immunity/immunity_by_age.py
+++ fig/starting_immunity/immunity_by_age.py
@@ -1,33 +1,39 @@
 #!/usr/bin/python
 
-fo = open('imm_vs_age.out','w')
-pop_size = 0
-imm = [[0,0,0,0,0] for i in range(101)]
+#expecting input files names immunity.1, immunity.2 ...
+for k in range(1,13):
+    print "running", k
+    fo = open('imm_vs_age.'+str(k),'w')
+    pop_size = 0
+    imm = [[0,0,0,0,0] for i in range(101)]
 
-header = True
-for line in file('immunity-yucatan.txt'):
-    '''
-    pid age imm1 imm2 imm3 imm4
-    1 31 1 0 1 0
-    2 29 1 1 0 1
-    3 10 1 0 0 0
-    4 32 1 1 0 0
-    5 30 1 0 0 1
-    '''
-    if header:
-        header = False
-        continue
+    header = True
+    for line in file('immunity.'+str(k)):
+        '''
+        pid age imm1 imm2 imm3 imm4
+        1 31 1 0 1 0
+        2 29 1 1 0 1
+        3 10 1 0 0 0
+        4 32 1 1 0 0
+        5 30 1 0 0 1
+        '''
+        if header:
+            header = False
+            continue
 
-    p = map(int,line.split())
+        p = map(int,line.split())
 
-    # haven't resolved what to do with people aged '999'
-    if p[1] > 100:
-        continue
-    imm[p[1]][0] += 1
-    for i, state in enumerate(p[2:6]):
-        imm[p[1]][i+1] += state
-        
-for age, vals in enumerate(imm):
-    fo.write(' '.join([str(age)] + map(str,vals)) + '\n')
+        # haven't resolved what to do with people aged '999'
+        if p[1] > 100:
+            continue
+        imm[p[1]][0] += 1
+        for i, state in enumerate(p[2:6]):
+            if state == 0:
+                imm[p[1]][i+1] += 0
+            else:
+                imm[p[1]][i+1] += 1
+            
+    for age, vals in enumerate(imm):
+        fo.write(' '.join([str(age)] + map(str,vals)) + '\n')
 
-fo.close()
+    fo.close()
