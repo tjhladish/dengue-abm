9e36b901a3ae93d122282fc4ebfd1f563825140a
Carl A. B. Pearson <cap10@ufl.edu>
2016-02-02 17:12:04 -0500

    updates to age distro + serolog processing

--- who_proj/processlogs.sh
+++ who_proj/processlogs.sh
@@ -2,9 +2,10 @@
 rm processed.csv
 rm scenarios.csv
 touch processed.csv
+touch scenarios.csv
 ls *.err-* | xargs cat | grep "seed, year, vaccine cohort age, seroprevalence" >> processed.csv
 ls *.err-* | xargs cat | grep "SCENARIO" >> scenarios.csv
-cut -d' ' -f2-16 scenarios.csv > seroscenarios.csv
+cut -d' ' -f2-17 scenarios.csv > seroscenarios.csv
 cut -d: -f2 processed.csv > reprocessed.csv
 echo "seed,year,vax_age,seroprevalence" > seroprevalence.csv 
 sed 's/^ *//g' < reprocessed.csv >> seroprevalence.csv
