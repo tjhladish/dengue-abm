9de43c7255fbed3bfd92ee11a0d783e22f682aa4
Thomas Hladish <tjhladish@ufl.edu>
2016-02-03 23:57:30 -0500

    Some tweaks to processlogs.sh

--- who_proj/processlogs.sh
+++ who_proj/processlogs.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-rm processed.csv
-rm scenarios.csv
-touch processed.csv
-ls *.err-* | xargs cat | grep "seed, year, vaccine cohort age, seroprevalence" >> processed.csv
-ls *.err-* | xargs cat | grep "SCENARIO" >> scenarios.csv
-cut -d' ' -f2-16 scenarios.csv > seroscenarios.csv
-cut -d: -f2 processed.csv > reprocessed.csv
+logdir="february2016/auto_output"
 echo "seed,year,vax_age,seroprevalence" > seroprevalence.csv 
-sed 's/^ *//g' < reprocessed.csv >> seroprevalence.csv
+grep "seed, year, vaccine cohort age, seroprevalence" $logdir/*.err-* | cut -d' ' -f7 >> seroprevalence.csv
+grep "SCENARIO" $logdir/*.err-* | cut -d' ' -f2-16 > seroscenarios.csv
+grep "COHORT" $logdir/*.err-* | cut -d, -f2- > cohort.csv
