f3ea3a73e23b0f4e15a86cbabdcc8d21f866037e
Thomas Hladish <tjhladish@ufl.edu>
2016-02-06 11:32:35 -0500

    Switching processlogs.sh to use a merged log file
    If we run jobs redundantly, we need to be able to filter through logs
    to get out the first record that was successfully completed and THEN
    run processlogs.sh, rather than running this on the raw logs.

--- who_proj/processlogs.sh
+++ who_proj/processlogs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-logdir="february2016/auto_output"
+logs="february2016/good_logs80"
 echo "seed,year,vax_age,seroprevalence" > seroprevalence.csv 
-grep "seed, year, vaccine cohort age, seroprevalence" $logdir/*.err-* | cut -d' ' -f7 >> seroprevalence.csv
-grep "SCENARIO" $logdir/*.err-* | cut -d' ' -f2-16 > seroscenarios.csv
-grep "COHORT" $logdir/*.err-* | cut -d, -f2- > cohort.csv
+grep "seed, year, vaccine cohort age, seroprevalence" $logs | cut -d' ' -f7 >> seroprevalence.csv
+grep "SCENARIO" $logs | cut -d' ' -f2-16 > seroscenarios.csv
+grep "COHORT" $logs | cut -d, -f2- > cohort.csv
