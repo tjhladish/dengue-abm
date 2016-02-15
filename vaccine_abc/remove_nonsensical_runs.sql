23350e1ff40bcbf6f8d183ea02b2c58fcaf746b6
Thomas Hladish <tjhladish@ufl.edu>
2016-02-15 18:08:52 -0500

    We didn't need to be running boosting with alternate target ages

--- vaccine_abc/remove_nonsensical_runs.sql
+++ vaccine_abc/remove_nonsensical_runs.sql
@@ -1,6 +1,11 @@
 delete from parameters where vac = 0 and catchup = 1;
 delete from parameters where vac = 0 and target != 9;
 delete from parameters where vac = 0 and (vac_waning != 0 or vac_boosting != 0);
+delete from parameters where vac_boosting != 0 and target != 9;
 delete from parameters where vac_waning = 0 and vac_boosting != 0;
+delete from parameters where vec_control = 0 and vec_scenario != 0;
+delete from parameters where vec_control != 0 and vac_waning != 0;
+delete from parameters where vec_control != 0 and target != 9;
 delete from jobs where serial not in (select serial from parameters);
 delete from metrics where serial not in (select serial from parameters);
+vacuum;
