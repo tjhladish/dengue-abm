91281070108f7f3956c306802c1d98cbf9d658e4
Carl A. B. Pearson <blank101@gmail.com>
2017-12-27 01:07:43 -0500

    fixed foi plot bug - copy paste error

--- irs_timing/figs/alt-eff.R
+++ irs_timing/figs/alt-eff.R
@@ -1,7 +1,7 @@
 
 args <- commandArgs(trailingOnly = TRUE)
 # args <- paste0("~/Dropbox/who/fig1_data/stopping-",c("baseline","interventions","eff"),".rds")
-# args <- c("~/Dropbox/who/fig1_data/foi-baseline.rds", "~/Dropbox/who/fig1_data/foi-interventions.rds", "~/Dropbox/who/fig1_data/foi-eff.rds")
+# args <- paste0("~/Dropbox/who/fig1_data/foi-",c("baseline","interventions","eff"),".rds")
 
 require(data.table)
 
@@ -12,13 +12,15 @@ ikey <- grep("^(imm|s|particle|year)", names(interventions.dt),invert=T, value=T
 
 join.dt <- interventions.dt[
   baseline.dt, on=bkey ][,
-  .(s, i.s),keyby=c(ikey, "particle", "year")
-][,
-  .(averted = i.s-s, denom = i.s, caverted = cumsum(i.s-s), cdenom=cumsum(i.s), year),
+  .(intcases = s, basecases = i.s), keyby=c(ikey, "particle", "year")
+]
+
+calc.dt <- join.dt[,
+  .(averted = basecases-intcases, denom = basecases, caverted = cumsum(basecases-intcases), cdenom=cumsum(basecases), year),
   by=c(ikey, "particle")
 ]
 
-eff.dt <- join.dt[!is.na(doy),
+eff.dt <- calc.dt[!is.na(doy),
   .(
     eff=ifelse(averted==0, 0, averted/denom),
     ceff=ifelse(caverted==0, 0, caverted/cdenom),
@@ -30,8 +32,8 @@ eff.dt <- join.dt[!is.na(doy),
 stat.eff.dt <- eff.dt[,{
     qs <- quantile(eff, probs = (0:4)/4, na.rm = T)
     q.mn <- mean(eff, na.rm = T)
-    cqs <- quantile(eff, probs = (0:4)/4, na.rm = T)
-    cq.mn <- mean(eff, na.rm = T)
+    cqs <- quantile(ceff, probs = (0:4)/4, na.rm = T)
+    cq.mn <- mean(ceff, na.rm = T)
     list(
       q.min = qs[1], q.lo = qs[2], q.med = qs[3], q.hi = qs[4], q.max = qs[5], q.mn=q.mn,
       cq.min = cqs[1], cq.lo = cqs[2], cq.med = cqs[3], cq.hi = cqs[4], cq.max = cqs[5], cq.mn=cq.mn
