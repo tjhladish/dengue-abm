d437bda01fb9df986b865511d8d14bb439ea3591
Carl A. B. Pearson <blank101@gmail.com>
2018-01-05 15:31:49 +0200

    updates to incorporate new foi, daily data in plots

--- irs_timing/figs/pro_vs_re_active.R
+++ irs_timing/figs/pro_vs_re_active.R
@@ -1,7 +1,7 @@
 rm(list=ls())
 
 args <- commandArgs(trailingOnly = T)
-# args <- c("~/Dropbox/who/fig1_data/campaign-timing-%.rds", "~/Dropbox/who/fig1_data/pro_vs_re_active.png")
+# args <- c("~/Dropbox/who/fig1_data/campaign-timing-prevalence.local.rds", "~/Dropbox/who/fig1_data/pro_vs_re_active.png")
 
 require(data.table)
 require(ggplot2)
@@ -9,9 +9,9 @@ require(ggplot2)
 plot.dt <- readRDS(args[1])
 timelvls <- c("None", "Proactive", "Reactive")
 
-start.day <- 125; end.day <- 900
-ymin <- 1; ymax <- 1e4; reps <- 5
-starts <- c(seq(from=152,by=365,length.out=reps),seq(from=305,by=365,length.out=reps))
+ymin <- 1; ymax <- 3e4; reps <- 4
+start.day <- 0; end.day <- start.day+365*(reps-1)
+starts <- c(seq(from=147,by=365,length.out=reps),seq(from=322,by=365,length.out=reps))
 irs.dt <- data.table(
   xmin=starts,
   xmax=starts+89+90,
@@ -45,18 +45,18 @@ pro.col <- "#0000ff"
 rea.col <- "#664400"
 
 png(args[2],height=1200,width=1000,res=180)
-ggplot(plot.dt,
+ggplot(plot.dt[foi == "Baseline Mosquito Population"],
        aes(fill=intervention)
 ) + theme_minimal() + theme(strip.text.y = element_text(angle=90)) +
-  coord_cartesian(xlim=c(125,850), ylim=c(ymin,ymax)) +
-  facet_grid(foi ~ ., scales = "free") +
+  coord_cartesian(xlim=c(start.day,end.day), ylim=c(ymin,ymax)) +
+#  facet_grid(foi ~ ., scales = "free") +
   #  geom_ribbon(aes(ymin=min, ymax=max, alpha="full range", color=NULL)) +
   geom_rect(aes(xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax, alpha=views[1]), data=irs.dt) +
   geom_text(aes(x=(xmax+xmin)/2, y=ifelse(intervention == "Proactive",ymax*.6,ymin/.6), label=intervention, color=intervention), data=irs.dt) +
-  geom_ribbon(aes(x=day, ymin=lo, ymax=hi, alpha=views[2])) +
+  geom_ribbon(aes(x=day-365, ymin=lo, ymax=hi, alpha=views[2])) +
   #  geom_line(aes(y=ave, linetype="average", alpha="line")) +
-  geom_line(aes(x=day, y=md, linetype="median", alpha=views[3], color=intervention)) +
-  scale_y_log10(name="# Active infections") +
+  geom_line(aes(x=day-365, y=md, linetype="median", alpha=views[3], color=intervention)) +
+  scale_y_log10(name="# Active infections (log scale)") +
   scale_alpha_manual(name="Measure", limits=views, values = c(0.1,0.2,1)) +
   scale_color_manual(name="Intervention", values=c(None='black',Proactive=pro.col,Reactive=rea.col)) +
   scale_fill_manual(name="Intervention", values=c(None='black',Proactive=pro.col,Reactive=rea.col)) +
