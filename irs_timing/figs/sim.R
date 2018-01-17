fdae22f00ff008667c95c9bc5abd034b82b6c2f6
Thomas Hladish <tjhladish@ufl.edu>
2018-01-16 22:59:00 -0500

    Tweaking fig scripts for new daily, R0 data

--- irs_timing/figs/sim.R
+++ irs_timing/figs/sim.R
@@ -2,7 +2,8 @@ require(data.table)
 
 args <- commandArgs(trailingOnly = TRUE)
 
-sim.raw = fread(args[1], header=F, col.names=c('serial','day','intro','local','intro_prev','local_prev'))
+#sim.raw = fread(args[1], header=F, col.names=c('serial','day','intro','local','intro_prev','local_prev'))
+sim.raw = fread(args[1], header=T)
 sim.raw = sim.raw[day<=50004,] # end before 2016
 # #sim.raw = sim.raw[day>46354,]  # start with 2006
 
