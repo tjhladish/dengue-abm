836dd649ba0db65a46fa987360143edd0a084780
Carl A. B. Pearson <blank101@gmail.com>
2018-05-08 22:34:35 +0200

    the extra intervention studies

---
+++ exp/irs_timing/figs/extra-interventions.R
@@ -0,0 +1,66 @@
+require(data.table)
+require(RSQLite)
+
+args <- paste0(
+  "~/Dropbox/who/",
+  c("irs_results-refit2.sqlite",
+    "irs_results-efficacy_sensitivity.sqlite",
+    "fig1_data/interventions.rds")
+)
+
+args <- commandArgs(trailingOnly = TRUE)
+
+drv = dbDriver("SQLite")
+
+tarcols <- sprintf("s%02d",0:9)
+
+qry <- sprintf("SELECT
+  timing+1 AS doy, vc_coverage*100 AS coverage,
+  campaign_duration AS duration, eff_days AS durability, vc_efficacy AS efficacy,
+  posterior AS particle,
+  %s
+  FROM met M
+  JOIN par P ON P.serial = M.serial
+  JOIN job J ON J.serial = M.serial
+  WHERE status = 'D'
+  AND vector_control = 1 
+  AND strat_years != 10;",
+  paste0("M.",tarcols,collapse=", ")
+)
+
+eff.dt <- rbindlist(lapply(head(args,-1), function(dbpath) {
+  db = dbConnect(drv, dbpath, flags=SQLITE_RO)
+  temp.dt <- data.table(dbGetQuery(db, qry))
+  dbDisconnect(db)
+  return(temp.dt)
+}))
+
+#eff.dt[, particle := floor(serial / 954)]
+
+q <- parse(text=paste0(".(cases10=",paste(tarcols, collapse = "+"),")"))
+
+tmp <- eff.dt[,
+  eval(q),
+  by=.(doy, coverage, duration, durability, efficacy, particle)
+]
+tmp[duration == 2, duration := 365]
+tmp[duration == 1, duration := 90]
+tmp[duration == 0, duration := 1]
+
+# db = dbConnect(drv, args[2], flags=SQLITE_RO)
+# d2 <- data.table(dbGetQuery(db,
+#   sprintf("SELECT timing+1 AS doy, vc_coverage*100 AS coverage, 90 AS duration,
+#   eff_days AS durability, %s
+#   FROM met M
+#   JOIN par P ON P.serial = M.serial
+#   JOIN job J ON J.serial = M.serial
+#   WHERE status = 'D' and vector_control = 1;", paste0("M.",tarcols,collapse=", "))
+# ))
+# d2[, particle := 1:.N-1L, by=list(doy, durability)]
+# d2 <- d2[,
+#   eval(q),
+#   by=.(doy, coverage, duration, durability, particle)
+# ]
+# 
+# store <- rbind(tmp, d2)
+saveRDS(tmp, tail(args, 1))
