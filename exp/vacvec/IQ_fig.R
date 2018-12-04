6de32b942ca2cddece1a6aedf2c6faa3092c7ecc
Carl A. B. Pearson <blank101@gmail.com>
2018-12-04 12:15:43 +0200

    vacvec SI IQ fig formatting tweaks

--- exp/vacvec/IQ_fig.R
+++ exp/vacvec/IQ_fig.R
@@ -17,10 +17,23 @@ stats.dt[vc == 0, vc_coverage := 0]
 stats.dt[, vac_mech := factor(ifelse(vac==1,c("cmdvi","trad")[vac_mech+1],"none"))]
 stats.dt[, catchup := factor(c("none","catchup")[catchup+1])]
 
-p<-ggplot(stats.dt) + aes(x=year+1, y=med, ymin=IQlo, ymax=IQhi, fill=vac_mech, color=vac_mech) +
-  facet_grid(catchup ~ vc_coverage) +
-  geom_ribbon(mapping = aes(color=NULL), alpha=.5) + geom_line() +
-  theme_minimal()
+flabs <- labeller(
+  catchup=c(none="None", catchup="Catchup"),
+  vc_coverage=c(`0`="0%",`25`="25%",`50`="50%",`75`="75%")
+)
+
+p <- ggplot(stats.dt) +
+  aes(x=year+1, y=med, ymin=IQlo, ymax=IQhi, fill=vac_mech) +
+  facet_grid(catchup ~ vc_coverage, labeller = flabs) +
+  geom_ribbon(alpha=.5) + geom_line(mapping=aes(color=vac_mech), show.legend = F) +
+  theme_minimal() +
+  scale_x_continuous("Year", expand = c(0,0)) +
+  scale_y_continuous("Effectiveness") +
+  labs(color="Vaccine", fill="Vaccine") + ggtitle("Vector Control Coverage") +
+  theme(
+    panel.spacing = unit(15,"pt"),
+    plot.title = element_text(size=rel(.75), hjust = 0.5, vjust=0)
+  )
 
 ggsave(
   tail(args,1), p, device = "png",
