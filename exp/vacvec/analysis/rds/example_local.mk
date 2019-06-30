b6220212b9708d26d39c55ecd8ff8dc2fc4e1c1f
Carl A. B. Pearson <blank101@gmail.com>
2019-06-30 03:55:17 -0400

    local.mk template

---
+++ exp/vacvec/analysis/rds/example_local.mk
@@ -0,0 +1,10 @@
+DROPBOXLOC ?= ...
+REFDB ?= $(DROPBOXLOC)/vacvec-new_yuc.sqlite
+FOIDB ?= $(DROPBOXLOC)/vacvec-new_yuc-alt_foi.sqlite
+LAGDB ?= $(DROPBOXLOC)/vacvec-new_yuc-ivn_lag258-reduced_catchup.sqlite
+FOISIDEDB ?= $(DROPBOXLOC)/mpeak_incidence_response-expanded.sqlite
+FOISIDETGZ ?= $(DROPBOXLOC)/mpeak_intros-expanded.out
+ABCDB ?= $(DROPBOXLOC)/new_yuc_posterior.sqlite
+TSTDB ?= $(DROPBOXLOC)/vacvec-new_yuc-CYD_TDV-seropos_only.sqlite
+TSTFOIDB ?= $(DROPBOXLOC)/vacvec-new_yuc-alt_foi-CYD_TDV-seropos_only.sqlite
+TESTDB ?= $(DROPBOXLOC)/vacvec-CYD_TDV-w_test.sqlite
