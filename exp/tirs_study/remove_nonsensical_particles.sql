3cfde94a8c55797671dcd9811338816531321ee6
Thomas Hladish <tjhladish@ufl.edu>
2022-05-20 01:03:32 -0400

    Updated TIRS config files

--- exp/tirs_study/remove_nonsensical_particles.sql
+++ exp/tirs_study/remove_nonsensical_particles.sql
@@ -1,5 +1,7 @@
---delete from par where vac = 0 and (vac_rate != 0);
---delete from job where serial not in (select serial from par);
---delete from met where serial not in (select serial from par);
-delete from par where wide_vc_campaign = 0 and vc_coverage != 1;
-delete from par where wide_vc_campaign = 1 and (vc_coverage != 0.5 and vc_coverage != 0.7 and vc_coverage != 0.9)
+delete from par where gen_vc = 0 and vc_coverage != 1.0;
+delete from par where gen_vc = 1 and not (vc_coverage = 0.5 or vc_coverage = 0.7 or vc_coverage = 0.9);
+delete from job where serial not in (select serial from par);
+delete from met where serial not in (select serial from par);
+update par set seed = (select posterior + 1 from job where par.serial = job.serial);
+vacuum;
+analyze;
